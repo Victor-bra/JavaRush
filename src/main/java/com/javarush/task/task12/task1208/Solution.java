5af49c7df42c2c46e3ddb4e560ced4b1b010946d
super <supervictorrap0865@gmail.com>
2022-10-06 23:02:33 +0300

    25 Day

---
+++ src/main/java/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1208;
+
+/*
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(){
+    }
+    public static void print(int x){
+    }
+    public static void print(String str){
+    }
+    public static void print(boolean b){
+    }
+    public static void print(char c){
+    }
+}
